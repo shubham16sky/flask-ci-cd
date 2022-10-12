8718553507d9fe433f68fea4af7ab8b5c126132c
Pranit Kumar <shubhampc16@gmail.com>
2022-10-12 22:52:32 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,6 +7,6 @@ if [ ! -z "$container" ]
 then
 	sudo docker stop testing && sudo docker rm testing
 else
-	sudo docker run --name "testing" --detach -p 5002:5002 flask-ci-cd
+	sudo docker run --name "testing" --detach -p 5005:5002 flask-ci-cd
 fi 
 
